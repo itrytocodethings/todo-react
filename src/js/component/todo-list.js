29cf8b45cc65a7e1e4ce5a36a142a90be6250034
itrytocodethings <imwaynebailey@gmail.com>
2022-02-03 21:31:00 -0500

    added task count and no task message

--- src/js/component/todo-list.js
+++ src/js/component/todo-list.js
@@ -9,7 +9,7 @@ export const Todo = () => {
 			<h1 className="display-1 text-center">todo</h1>
 			<div className="row mt-3">
 				<div className="col-12 col-sm-3 mx-auto">
-					<div className="todo paper">
+					<div className="todo paper pb-0 px-1">
 						<input
 							className="form-control"
 							type="text"
@@ -36,15 +36,38 @@ export const Todo = () => {
 									setListItem([...listItems]);
 								}
 							}}>
-							{listItems.map((task, i) => (
-								<li key={i} id={i} className="list-group-item">
-									{task}
-									<span className="trash">
-										<i className="far fa-trash-alt"></i>
-									</span>
-								</li>
-							))}
+							{listItems.length > 0 ? (
+								listItems.map((task, i) => (
+									<li
+										key={i}
+										id={i}
+										className="list-group-item">
+										{task}
+										<span className="trash">
+											<i className="far fa-trash-alt"></i>
+										</span>
+									</li>
+								))
+							) : (
+								<small className="text-muted">
+									<em>no tasks</em> &#128546;
+								</small>
+							)}
 						</ul>
+						<div className="paper-foot p-0 m-0">
+							{listItems.length > 0 ? (
+								<small className="count text-muted">
+									<em>
+										{listItems.length}{" "}
+										{listItems.length > 1
+											? `items`
+											: `item`}{" "}
+										left
+									</em>{" "}
+									&#128521;
+								</small>
+							) : null}
+						</div>
 					</div>
 				</div>
 			</div>
