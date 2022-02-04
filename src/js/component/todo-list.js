261042b5276e98bc07898f9534b98e4bf7867a5c
itrytocodethings <imwaynebailey@gmail.com>
2022-02-03 21:07:22 -0500

    updated list items to have id of their index in array for easy identification in the onclick to remove the item event

--- src/js/component/todo-list.js
+++ src/js/component/todo-list.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 
 export const Todo = () => {
-	const [todoVal, setTodoVal] = useState();
+	const [todoVal, setTodoVal] = useState("");
 	const [listItems, setListItem] = useState([]);
 
 	return (
 		<div>
 			<h1 className="display-1 text-center">todo</h1>
 			<div className="row mt-3">
-				<div className="col-12 col-sm-6 mx-auto">
+				<div className="col-12 col-sm-3 mx-auto">
 					<div className="todo paper">
 						<input
 							className="form-control"
@@ -20,7 +20,7 @@ export const Todo = () => {
 							onChange={(e) => setTodoVal(e.target.value)}
 							onKeyUp={(e) => {
 								if (e.keyCode == 13) {
-									setListItem([...listItems, todoVal]);
+									setListItem([...listItems, todoVal.trim()]);
 									setTodoVal("");
 								}
 							}}
@@ -28,23 +28,16 @@ export const Todo = () => {
 						<ul
 							className="list-group py-2"
 							onClick={(e) => {
-								if (
-									e.target.nodeName == "I" ||
-									e.target.nodeName == "SPAN"
-								) {
-									setListItem(
-										[...listItems].filter(
-											(item) =>
-												!(item =
-													e.target.parentElement
-														.parentElement
-														.innerText == item)
-										)
+								if (e.target.nodeName == "I") {
+									listItems.splice(
+										e.target.parentElement.parentElement.id,
+										1
 									);
+									setListItem([...listItems]);
 								}
 							}}>
-							{listItems.map((task) => (
-								<li className="list-group-item">
+							{listItems.map((task, i) => (
+								<li key={i} id={i} className="list-group-item">
 									{task}
 									<span className="trash">
 										<i className="far fa-trash-alt"></i>
